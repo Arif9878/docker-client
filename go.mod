11877928878ab0d699317feb7d5b380f7fd07fd6
Arif9878 <arifboyz16@gmail.com>
2021-07-04 11:20:18 +0700

    docker client api

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Arif9878/docker-client
+
+go 1.16
+
+require (
+	github.com/Microsoft/hcsshim v0.8.18 // indirect
+	github.com/fsouza/go-dockerclient v1.7.3
+	github.com/opencontainers/runc v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"archive/tar"
+	"bytes"
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
 
 	docker "github.com/fsouza/go-dockerclient"
 )
@@ -9,18 +14,88 @@ import (
 func main() {
 	client, err := docker.NewClientFromEnv()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	imgs, err := client.ListImages(docker.ListImagesOptions{All: false})
+	ctx := context.Background()
+
+	dockerfile := "docker/Dockerfile"
+
+	// Create a buffer
+	buf := new(bytes.Buffer)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	// Create a filereader
+	dockerFileReader, err := os.Open(dockerfile)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	for _, img := range imgs {
-		fmt.Println("ID: ", img.ID)
-		fmt.Println("RepoTags: ", img.RepoTags)
-		fmt.Println("Created: ", img.Created)
-		fmt.Println("Size: ", img.Size)
-		fmt.Println("VirtualSize: ", img.VirtualSize)
-		fmt.Println("ParentId: ", img.ParentID)
+
+	// Read the actual Dockerfile
+	readDockerFile, err := ioutil.ReadAll(dockerFileReader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Make a TAR header for the file
+	tarHeader := &tar.Header{
+		Name: dockerfile,
+		Size: int64(len(readDockerFile)),
+	}
+
+	// Writes the header described for the TAR file
+	err = tw.WriteHeader(tarHeader)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Writes the dockerfile data to the TAR file
+	_, err = tw.Write(readDockerFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dockerFileTarReader := bytes.NewReader(buf.Bytes())
+	opts := docker.BuildImageOptions{
+		Context:      ctx,
+		Name:         "hello-world",
+		Dockerfile:   dockerfile,
+		InputStream:  dockerFileTarReader,
+		OutputStream: bytes.NewBuffer(nil),
+		Pull:         false,
+	}
+	if err := client.BuildImage(opts); err != nil {
+		log.Fatal(err)
+	}
+
+	portBindings := map[docker.Port][]docker.PortBinding{
+		"80/tcp": {{HostIP: "0.0.0.0", HostPort: "8080"}}}
+
+	createContHostConfig := docker.HostConfig{
+		PortBindings:    portBindings,
+		PublishAllPorts: true,
+		Privileged:      false,
+	}
+
+	exposedCadvPort := map[docker.Port]struct{}{
+		"80/tcp": {}}
+
+	createContConf := docker.Config{
+		ExposedPorts: exposedCadvPort,
+		Image:        "hello-world:latest",
+	}
+
+	optsContainer := docker.CreateContainerOptions{
+		Context:    ctx,
+		Name:       "hello-world",
+		Config:     &createContConf,
+		HostConfig: &createContHostConfig,
+	}
+	container, err := client.CreateContainer(optsContainer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := client.StartContainerWithContext(container.ID, nil, ctx); err != nil {
+		log.Fatal(err)
 	}
 }
